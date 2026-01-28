17c9e24aa6abbda6ad3809e547dffe8be841ac3b
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-01-29 01:10:22 +0530

    validate request

---
+++ udemy/ch12_validate_request/app/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+##Create or Insert Data
+# @app.post("/product")
+# async def create_product(new_product:dict):
+#     return new_product
+
+## With Pydantic 
+# # Define the Product model
+class Product(BaseModel):
+    id:int
+    name:str
+    price:float
+    stock:int | None=None
+
+# @app.post("/product")
+# async def create_product(new_product:Product):
+#     return new_product
+
+# @app.post("/product")
+# async def create_product(new_product:Product):
+#     print(Product)
+#     print(new_product)
+#     print(new_product.id)
+#     print(new_product.name)
+#     print(new_product.price)
+#     print(new_product.stock)
+#     return new_product
+
+# @app.post("/product")
+# async def create_product(new_product:Product):
+#     product_dict = new_product.model_dump()
+#     price_with_tax = new_product.price+(new_product.price*18/100)
+#     product_dict.update({"price_with_tax":price_with_tax})
+#     return product_dict
+
+## Combine Request Body with Path parameters
+# @app.put("/products/{product_id}")
+# async def update_product(product_id:int,new_updated_product:Product):
+#     return {"product_id":product_id,"new_updated_product":new_updated_product}
+
+## Adding Query Parameter
+@app.put("/products/{product_id}")
+async def update_product(product_id:int,new_updated_product:Product,discount:float | None=None):
+    return {"product_id":product_id,"new_updated_product":new_updated_product}
