b2ccd62587cb0065f827e639c14246318dfa9948
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-02-02 00:36:02 +0530

    nested pydantic

--- udemy/ch14_pydantic/nested/main.py
+++ udemy/ch14_pydantic/nested/main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel,Field
 app = FastAPI()
 
 ## Nested BOdy Model
+# SubModel
 class Category(BaseModel):
     name:str = Field(
         title="category name ",
@@ -18,3 +19,61 @@ class Category(BaseModel):
         max_length=50
 
     )
+
+# #Model use Submodel
+# class Product(BaseModel):
+#     name:str = Field(
+#         title="product name ",
+#         description="The name of the product",
+#         max_length=100,
+#         min_length=3,
+#         pattern="^[A-Za-z)-9 ]+$"
+#     )
+#     price:float=Field(
+#         title="product Price",
+#         gt=5,
+#         description="The price of the product in USD,must be greater than the zero"
+#     )    
+#     stock:int | None =Field(
+#         default=None,
+#         gt=0,
+#         description="The number of item in stock, must be non-negative"
+#     )
+#     category:Category | None = Field(default=None,
+#                                     title="product Category",
+#                                      description="The category to which the product belongs" )
+
+# @app.post("/product")
+# async def create_product(product:Product):
+#     print(f'"product":{product}')
+#     return {"product":product}
+
+### Attribute with lists of submodels
+class Product(BaseModel):
+    name:str = Field(
+        title="product name ",
+        description="The name of the product",
+        max_length=100,
+        min_length=3,
+        pattern="^[A-Za-z)-9 ]+$"
+    )
+    price:float=Field(
+        title="product Price",
+        gt=5,
+        description="The price of the product in USD,must be greater than the zero"
+    )    
+    stock:int | None =Field(
+        default=None,
+        gt=0,
+        description="The number of item in stock, must be non-negative"
+    )
+    category:list[Category] | None=Field(
+                                    default=None,
+                                    title="product Category",
+                                    description="The category to which the product belongs"
+                                    )
+
+@app.post("/product")
+async def create_product(product:Product):
+    print(f'"product":{product}')
+    return {"product":product}
