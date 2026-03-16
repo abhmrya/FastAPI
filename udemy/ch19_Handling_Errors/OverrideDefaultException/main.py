597110d6da09e715552e11ec62c501f6d49bebac
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-03-16 15:37:24 +0530

    exception end

---
+++ udemy/ch19_Handling_Errors/OverrideDefaultException/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+# fruits = {
+#     "apple":"A juice fruit",
+#     "Banana":"A yellow delight"
+# }
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request,exc:RequestValidationError):
+    return PlainTextResponse(str(exc),status_code=400)
+
+@app.get("/item/{item_id}")
+async def read_item(item_id:int):
+    return item_id
